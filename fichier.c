5d968e4ea80d06c74b194795756a5acca0a83dea
mohamedazizbenchedly <mohamedaziz.benchedly@esprit.tn>
2020-10-25 12:59:19 +0100

    verifier la parite

--- fichier.c
+++ fichier.c
@@ -3,7 +3,15 @@ int main()
 { 
 int x = 0; 
 printf("x = "); 
-scanf("%d", x); 
+scanf("%d", x);
+printf("bonjour tp1\n");
+printf("donner entier a \n");
+s=a%2; 
+if (s==0)
+printf("votre entier est paire \n");
+else 
+printf(" n est pas paire \n");
 return (0); 
-} 
+}
+ 
 
